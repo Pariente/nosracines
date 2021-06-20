5e4a5d5001c96290406df45b8493ac3e100e28a2
pariente <parientethomas@gmail.com>
2021-06-20 20:32:37 +0200

    document viewer

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,6 +2,9 @@ class DocumentsController < ApplicationController
   def show
     @document = Document.find(params[:id])
     @document_files = @document.document_files
+    unless params[:document_file].nil?
+      @document_file = DocumentFile.find(params[:document_file])
+    end
   end
 
   def index
